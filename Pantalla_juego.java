87f31e763abf508b4d26a1e83b08e64ffb7cd3df
JuanFB04 <juanigfigueroa@gmail.com>
2023-07-20 07:30:18 -0600

    Primer commit

---
+++ Pantalla_juego.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Pantalla_juego here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pantalla_juego extends World
+{
+
+    /**
+     * Constructor for objects of class Pantalla_juego.
+     * 
+     */
+    public Pantalla_juego()
+    {    
+        super(500, 500, 1); 
+        setBackground("Pantalla_juego.png");
+        }
+    }
+
---
+++ Tortuga.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Tortuga here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tortuga extends Actor
+{
+    /**
+     * Act - do whatever the Tortuga wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        setLocation(0,0);
+        if(Greenfoot.isKeyDown("a")){
+            move(5);
+        }
+    }
+}
